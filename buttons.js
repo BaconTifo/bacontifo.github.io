5fc9b05b36440ef14f31d4ee357757f96bcf4fbe
BaconTifo <bacontifo1@gmail.com>
2025-04-16 21:28:46 +0700

    Delete buttons.js

--- buttons.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.button').forEach(button => {
-    button.addEventListener('click', function() {
-      const targetSection = document.querySelector(button.getAttribute('data-target'));
-      targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
